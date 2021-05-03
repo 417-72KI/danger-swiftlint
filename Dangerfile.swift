242a6191b5abb01a92661abefccc3a3bc7e75dd5
417-72KI <417.72ki@gmail.com>
2021-05-04 01:21:18 +0900

    print versions in PR's verifying

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,20 @@
-import Danger 
+import Danger
+
+// fileImport: DangerExtensions/Shell.swift
 
 let danger = Danger()
 
 danger.message("Validation passed! 🎉")
+
+if let swiftVersion = shell("swift", "--version") {
+    danger.message(swiftVersion)
+}
+if let dangerSwiftVersion = shell("danger-swift", "--version") {
+    danger.message("danger-swift: \(dangerSwiftVersion)")
+}
+if let dangerJSVersion = shell("danger-js", "--version") {
+    danger.message("danger-js: \(dangerJSVersion)")
+}
+if let swiftLintVersion = shell("swiftlint", "version") {
+    danger.message("SwiftLint: \(swiftLintVersion)")
+}
