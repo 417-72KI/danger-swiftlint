d8dad7b82063f0794014b331c8896ffa4bfd2742
417-72KI <417.72ki@gmail.com>
2021-09-23 01:12:58 +0900

    add argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -lt 1 ]; then
     echo -e "\e[31mUsage: $0 [cmd]\e[m"
@@ -7,4 +7,10 @@ fi
 
 show-versions
 
-danger-swift $@
+if [[ "${INPUT_DANGERFILE}" == '' ]]; then
+    DANGERFILE_ARGS=''
+else
+    DANGERFILE_ARGS="--dangerfile ${INPUT_DANGERFILE}"
+fi
+
+danger-swift $@ $DANGERFILE_ARGS
