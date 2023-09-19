5d3b267004605c4b9ab960fbae3e4a7ce944c312
417-72KI <417.72ki@gmail.com>
2023-09-19 23:45:47 +0900

    set exit_code for dumping versions

--- versions.sh
+++ versions.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 swift --version | grep 'Swift version'
-echo "SwiftLint version $(swiftlint version)"
-echo "danger-js version $(danger --version)"
-echo "danger-swift version $(danger-swift --version)"
+EXIT_CODE=$?
+SWIFTLINT_VERSION=$(swiftlint version)
+EXIT_CODE=$((EXIT_CODE|$?))
+DANGER_JS_VERSION=$(danger --version)
+EXIT_CODE=$((EXIT_CODE|$?))
+DANGER_SWIFT_VERSION=$(danger-swift --version)
+EXIT_CODE=$((EXIT_CODE|$?))
+
+echo "SwiftLint version ${SWIFTLINT_VERSION}"
+echo "danger-js version ${DANGER_JS_VERSION}"
+echo "danger-swift version ${DANGER_SWIFT_VERSION}"
+
+exit $EXIT_CODE
